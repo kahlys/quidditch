3c9d5dd3dc244955fd3db54546e76be87b83057e
kahlys <youennlaborde@live.fr>
2023-01-29 23:48:19 +0100

    chore: backend docker compose

--- backend/cmd/qmserv/main.go
+++ backend/cmd/qmserv/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"log"
+	"flag"
 	"net/http"
+	"os"
+
+	"go.uber.org/zap"
 
 	"github.com/kahlys/quidditch/backend/api"
 )
 
-func main() {
-	router := api.Handler()
+var fAddr = flag.String("addr", ":80", "listening address")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	serv := http.Server{
+		Addr:    *fAddr,
+		Handler: api.Handler(),
+	}
+
+	logger.Sugar().Infow("Server listening", "addr", *fAddr)
+	err = serv.ListenAndServe()
+	logger.Sugar().Fatalw("Server stopped", "err", err.Error())
 }
