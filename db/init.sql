c57a1c8925859325302bf306b0216df532f07936
kahlys <youennlaborde@live.fr>
2023-02-09 00:43:31 +0100

    feat(back): init database schema

--- db/init.sql
+++ db/init.sql
@@ -6,3 +6,52 @@ CREATE TABLE users (
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   last_login TIMESTAMP
 );
+
+CREATE TABLE teams (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  owner_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE players (
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  nationality TEXT NOT NULL,
+  power INTEGER NOT NULL,
+  stamina INTEGER NOT NULL,
+  position TEXT NOT NULL,
+  team_id INTEGER REFERENCES teams(id)
+);
+
+CREATE TABLE seasons (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	first_place TEXT,
+	second_place TEXT,
+	third_place TEXT
+);
+
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY,
+  home_team_id INTEGER REFERENCES teams(id),
+  away_team_id INTEGER REFERENCES teams(id),
+  home_team_score INTEGER NOT NULL,
+  away_team_score INTEGER NOT NULL,
+  season_id INTEGER REFERENCES seasons(id)
+);
+
+CREATE TABLE season_standings (
+  id SERIAL PRIMARY KEY,
+  team_id INTEGER REFERENCES teams(id),
+  season_id INTEGER REFERENCES seasons(id),
+  points INTEGER NOT NULL
+);
+
+CREATE TABLE settings (
+  key TEXT NOT NULL PRIMARY KEY,
+  value TEXT NOT NULL
+);
+
+INSERT INTO settings (key, value) VALUES ('season_counter', '0');
+INSERT INTO settings (key, value) VALUES ('schema_version', '1');
